0a4794f82cfc044f9102fdb670664ff0b19f94a6
Dinesh <108384004+ds4914@users.noreply.github.com>
2024-09-05 18:38:48 +0530

    Update snackbar.dart

--- lib/shared/utils/snackbar.dart
+++ lib/shared/utils/snackbar.dart
@@ -1,3 +1,4 @@
+import 'package:stoxplay/main.dart';
 import 'package:stoxplay/shared/shared.dart';
 
 void showSnackBar({
@@ -8,6 +9,26 @@ void showSnackBar({
 }) {
   ScaffoldMessenger.of(context).hideCurrentSnackBar();
   ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Align(
+        alignment: Alignment.centerLeft,
+        child: TextView(
+          text: message,
+        ),
+      ),
+      backgroundColor: backgroundColor,
+      duration: Duration(seconds: durationInSeconds),
+    ),
+  );
+}
+
+void showSnackBarUsingKey({
+  required String message,
+  Color backgroundColor = AppColors.colorSecondary, // Default background color
+  int durationInSeconds = 3, // Default duration
+}) {
+  snackBarKey.currentState!.removeCurrentSnackBar();
+  snackBarKey.currentState!.showSnackBar(
     SnackBar(
       content: Align(
         alignment: Alignment.centerLeft,
