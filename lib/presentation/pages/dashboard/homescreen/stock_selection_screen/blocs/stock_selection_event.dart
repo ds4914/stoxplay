8fd5bdbc65401f18d165df056fabd80a16d441f4
Dinesh <108384004+ds4914@users.noreply.github.com>
2024-09-05 18:37:53 +0530

    Update stock_selection_event.dart

--- lib/presentation/pages/dashboard/homescreen/stock_selection_screen/blocs/stock_selection_event.dart
+++ lib/presentation/pages/dashboard/homescreen/stock_selection_screen/blocs/stock_selection_event.dart
@@ -3,13 +3,58 @@ part of 'stock_selection_bloc.dart';
 @immutable
 sealed class StockSelectionEvent {}
 
-class AddStockToList extends StockSelectionEvent{
-  final List<SelectedStock>? selectedStockList;
-  final Map<String,dynamic>? stockList;
-  AddStockToList({this.selectedStockList, this.stockList});
+class StockSelectionAddEvent extends StockSelectionEvent {
+  final int? index;
+  final bool? isUp;
+  final List<int>? upSelectedStock;
+  final List<int>? downSelectedStock;
+  final List<bool>? moreButtonClicked;
+  bool? showSnackBar = false;
+
+  StockSelectionAddEvent(
+      {this.upSelectedStock, this.downSelectedStock, this.moreButtonClicked, this.index, this.isUp, this.showSnackBar});
 }
 
-class AddStockEvent extends StockSelectionEvent{
-  final bool? isSelected;
-  AddStockEvent(this.isSelected);
+class StockSelectionRemoveEvent extends StockSelectionEvent {
+  final int? index;
+  final List<int>? upSelectedStock;
+  final List<int>? downSelectedStock;
+  final List<bool>? moreButtonClicked;
+  StockSelectionRemoveEvent({this.upSelectedStock, this.downSelectedStock, this.moreButtonClicked, this.index});
+}
+
+class OnMoreClickedEvent extends StockSelectionEvent {
+  final int? index;
+  final List<int>? upSelectedStock;
+  final List<int>? downSelectedStock;
+  final List<bool>? moreButtonClicked;
+  bool? showSnackBar = false;
+  OnMoreClickedEvent({this.upSelectedStock, this.showSnackBar, this.downSelectedStock, this.moreButtonClicked, this.index});
+}
+
+class OnLeaderClickedEvent extends StockSelectionEvent {
+  int? leaderIndex;
+  final int? coLeaderIndex;
+  final int? viceLeaderIndex;
+  final int? index;
+
+  OnLeaderClickedEvent({this.index, this.leaderIndex, this.coLeaderIndex, this.viceLeaderIndex});
+}
+
+class OnCoLeaderClickedEvent extends StockSelectionEvent {
+  final int? leaderIndex;
+  int? coLeaderIndex;
+  final int? viceLeaderIndex;
+  final int? index;
+
+  OnCoLeaderClickedEvent({this.index, this.leaderIndex, this.coLeaderIndex, this.viceLeaderIndex});
+}
+
+class OnViceLeaderClickedEvent extends StockSelectionEvent {
+  final int? leaderIndex;
+  final int? coLeaderIndex;
+  int? viceLeaderIndex;
+  final int? index;
+
+  OnViceLeaderClickedEvent({this.index, this.leaderIndex, this.coLeaderIndex, this.viceLeaderIndex});
 }
