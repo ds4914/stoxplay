b85bdd4f79553431ed4c3ffbc10f1d6e5333b28e
Dinesh <108384004+ds4914@users.noreply.github.com>
2024-09-05 18:39:46 +0530

    Update common_stock_selection_card.dart

--- lib/presentation/pages/dashboard/homescreen/stock_selection_screen/widgets/common_stock_selection_card.dart
+++ lib/presentation/pages/dashboard/homescreen/stock_selection_screen/widgets/common_stock_selection_card.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:stoxplay/shared/widgets/common_icon_for_position.dart';
 
 import '../../../../../../shared/shared.dart';
@@ -7,9 +8,9 @@ import '../blocs/stock_selection_bloc.dart';
 class CommonStockSelectionCard extends StatelessWidget {
   final int index;
   final Map<String, dynamic> data;
-  final int? leaderIndex;
-  final int? coLeaderIndex;
-  final int? viceLeaderIndex;
+  int? leaderIndex;
+  int? coLeaderIndex;
+  int? viceLeaderIndex;
   final void Function()? onUpButtonTap;
   final void Function()? onTap;
   final void Function()? onDownButtonTap;
@@ -17,12 +18,19 @@ class CommonStockSelectionCard extends StatelessWidget {
   final void Function()? onLClick;
   final void Function()? onVLClick;
   final void Function()? onCLClick;
-  List<int> upSelectedStock = [];
-  List<bool> roleRowShow;
-  List<int> downSelectedStock = [];
+  final List<int>? upSelectedStock;
+  final List<int>? downSelectedStock;
+  final List<bool>? moreButtonClicked;
+  final StockSelectionBloc leaderBloc;
+  final StockSelectionBloc coLeaderBloc;
+  final StockSelectionBloc viceLeaderBloc;
+
   CommonStockSelectionCard({
     super.key,
     required this.index,
+    required this.leaderBloc,
+    required this.coLeaderBloc,
+    required this.viceLeaderBloc,
     required this.data,
     required this.onLClick,
     required this.leaderIndex,
@@ -31,12 +39,12 @@ class CommonStockSelectionCard extends StatelessWidget {
     required this.onVLClick,
     required this.onCLClick,
     required this.onUpButtonTap,
-    required this.roleRowShow,
     required this.onMoreTap,
     required this.onTap,
-    required this.upSelectedStock,
-    required this.downSelectedStock,
     required this.onDownButtonTap,
+    this.upSelectedStock,
+    this.downSelectedStock,
+    this.moreButtonClicked,
   });
 
   @override
@@ -49,16 +57,16 @@ class CommonStockSelectionCard extends StatelessWidget {
             elevation: 3.0,
             child: Container(
               decoration: BoxDecoration(
-                  color: upSelectedStock.contains(index)
+                  color: upSelectedStock!.contains(index)
                       ? AppColors.lightGreen
-                      : downSelectedStock.contains(index)
+                      : downSelectedStock!.contains(index)
                           ? AppColors.lightRed
                           : AppColors.white,
                   borderRadius: BorderRadius.circular(10.r),
                   border: Border.all(
-                      color: upSelectedStock.contains(index)
+                      color: upSelectedStock!.contains(index)
                           ? AppColors.colorOnline
-                          : downSelectedStock.contains(index)
+                          : downSelectedStock!.contains(index)
                               ? AppColors.red
                               : AppColors.color999999)),
               child: Row(
@@ -72,8 +80,7 @@ class CommonStockSelectionCard extends StatelessWidget {
                                 'assets/images/app_icon.png',
                                 height: 40.h,
                                 width: 40.w,
-                              ).paddingSymmetric(
-                                  horizontal: 5.w, vertical: 5.h))
+                              ).paddingSymmetric(horizontal: 5.w, vertical: 5.h))
                           .paddingSymmetric(horizontal: 10.w),
                       Row(
                         children: [
@@ -94,85 +101,164 @@ class CommonStockSelectionCard extends StatelessWidget {
                       ).paddingTop(30.h),
                     ],
                   ),
-                  roleRowShow[index]
+                  moreButtonClicked![index]
                       ? Row(
                           children: [
                             GestureDetector(
-                              onTap: () {},
-                              child: Container(
-                                  height: 30.h,
-                                  width: 30.w,
-                                  decoration: BoxDecoration(
-                                      color: AppColors.white,
-                                      shape: BoxShape.circle,
-                                      boxShadow: [
-                                        BoxShadow(
-                                            color: AppColors.blue,
-                                            spreadRadius: 1.0,
-                                            blurRadius: 1.0)
-                                      ],
-                                      border:
-                                          Border.all(color: AppColors.blue)),
-                                  child: Center(
-                                    child: TextView(
-                                      text: "L",
-                                      fontSize: 15.sp,
-                                      fontWeight: FontWeight.bold,
-                                      fontColor: AppColors.color666666,
-                                    ),
-                                  )),
+                              onTap: () {
+                                leaderBloc.add(OnLeaderClickedEvent(
+                                    leaderIndex: leaderIndex,
+                                    index: index,
+                                    coLeaderIndex: coLeaderIndex,
+                                    viceLeaderIndex: viceLeaderIndex));
+                              },
+                              child: BlocBuilder<StockSelectionBloc, StockSelectionState>(
+                                bloc: leaderBloc,
+                                builder: (context, state) {
+                                  if (state is LeaderClickedState) {
+                                    print('object==>${state.leaderIndex}');
+                                    leaderIndex = state.leaderIndex;
+                                    return Container(
+                                        height: 30.h,
+                                        width: 30.w,
+                                        decoration: BoxDecoration(
+                                            color: state.leaderIndex == index ? AppColors.gradientOne : AppColors.white,
+                                            shape: BoxShape.circle,
+                                            boxShadow: [BoxShadow(color: AppColors.blue, spreadRadius: 1.0, blurRadius: 1.0)],
+                                            border: Border.all(color: AppColors.blue)),
+                                        child: Center(
+                                          child: TextView(
+                                            text: "L",
+                                            fontSize: 15.sp,
+                                            fontWeight: FontWeight.bold,
+                                            fontColor: state.leaderIndex == index ? AppColors.white : AppColors.color666666,
+                                          ),
+                                        ));
+                                  }
+                                  return Container(
+                                      height: 30.h,
+                                      width: 30.w,
+                                      decoration: BoxDecoration(
+                                          color: AppColors.white,
+                                          shape: BoxShape.circle,
+                                          boxShadow: [BoxShadow(color: AppColors.blue, spreadRadius: 1.0, blurRadius: 1.0)],
+                                          border: Border.all(color: AppColors.blue)),
+                                      child: Center(
+                                        child: TextView(
+                                          text: "L",
+                                          fontSize: 15.sp,
+                                          fontWeight: FontWeight.bold,
+                                          fontColor: AppColors.color666666,
+                                        ),
+                                      ));
+                                },
+                              ),
                             ),
-                            SizedBox(width: 2.w,),
+                            SizedBox(
+                              width: 2.w,
+                            ),
                             GestureDetector(
-                              onTap: () {},
-                              child: Container(
-                                  height: 30.h,
-                                  width: 30.w,
-                                  decoration: BoxDecoration(
-                                      color: AppColors.white,
-                                      shape: BoxShape.circle,
-                                      boxShadow: [
-                                        BoxShadow(
-                                            color: AppColors.blue,
-                                            spreadRadius: 1.0,
-                                            blurRadius: 1.0)
-                                      ],
-                                      border:
-                                          Border.all(color: AppColors.blue)),
-                                  child: Center(
-                                    child: TextView(
-                                      text: "VL",
-                                      fontSize: 15.sp,
-                                      fontWeight: FontWeight.bold,
-                                      fontColor: AppColors.color666666,
-                                    ),
-                                  )),
+                              onTap: () {
+                                viceLeaderBloc.add(OnViceLeaderClickedEvent(
+                                    leaderIndex: leaderIndex,
+                                    index: index,
+                                    coLeaderIndex: coLeaderIndex,
+                                    viceLeaderIndex: viceLeaderIndex));
+                              },
+                              child: BlocBuilder<StockSelectionBloc, StockSelectionState>(
+                                bloc: viceLeaderBloc,
+                                builder: (context, state) {
+                                  if (state is ViceLeaderClickedState) {
+                                    print('object==>${state.viceLeaderIndex}');
+                                    viceLeaderIndex = state.viceLeaderIndex;
+                                    return Container(
+                                        height: 30.h,
+                                        width: 30.w,
+                                        decoration: BoxDecoration(
+                                            color: state.viceLeaderIndex == index ? AppColors.gradientOne : AppColors.white,
+                                            shape: BoxShape.circle,
+                                            boxShadow: [BoxShadow(color: AppColors.blue, spreadRadius: 1.0, blurRadius: 1.0)],
+                                            border: Border.all(color: AppColors.blue)),
+                                        child: Center(
+                                          child: TextView(
+                                            text: "VL",
+                                            fontSize: 15.sp,
+                                            fontWeight: FontWeight.bold,
+                                            fontColor: state.viceLeaderIndex == index ? AppColors.white : AppColors.color666666,
+                                          ),
+                                        ));
+                                  }
+                                  return Container(
+                                      height: 30.h,
+                                      width: 30.w,
+                                      decoration: BoxDecoration(
+                                          color: AppColors.white,
+                                          shape: BoxShape.circle,
+                                          boxShadow: [BoxShadow(color: AppColors.blue, spreadRadius: 1.0, blurRadius: 1.0)],
+                                          border: Border.all(color: AppColors.blue)),
+                                      child: Center(
+                                        child: TextView(
+                                          text: "VL",
+                                          fontSize: 15.sp,
+                                          fontWeight: FontWeight.bold,
+                                          fontColor: AppColors.color666666,
+                                        ),
+                                      ));
+                                },
+                              ),
                             ).paddingSymmetric(horizontal: 2.w),
-                            SizedBox(width: 2.w,),
+                            SizedBox(
+                              width: 2.w,
+                            ),
                             GestureDetector(
-                              onTap: () {},
-                              child: Container(
-                                  height: 30.h,
-                                  width: 30.w,
-                                  decoration: BoxDecoration(
-                                      color: AppColors.white,
-                                      shape: BoxShape.circle,
-                                      boxShadow: [
-                                        BoxShadow(
-                                            color: AppColors.blue,
-                                            spreadRadius: 1.0,
-                                            blurRadius: 1.0)
-                                      ],
-                                      border:
-                                          Border.all(color: AppColors.blue)),
-                                  child: Center(
-                                    child: TextView(
-                                      text: "CL",
-                                      fontSize: 15.sp,
-                                      fontWeight: FontWeight.bold,
-                                      fontColor: AppColors.color666666,
-                                    ),
-                                  )),
+                              onTap: () {
+                                coLeaderBloc.add(OnCoLeaderClickedEvent(
+                                    leaderIndex: leaderIndex,
+                                    index: index,
+                                    coLeaderIndex: coLeaderIndex,
+                                    viceLeaderIndex: viceLeaderIndex));
+                              },
+                              child: BlocBuilder<StockSelectionBloc, StockSelectionState>(
+                                bloc: coLeaderBloc,
+                                builder: (context, state) {
+                                  if (state is CoLeaderClickedState) {
+                                    print('object==>${state.coLeaderIndex}');
+                                    coLeaderIndex = state.coLeaderIndex;
+                                    return Container(
+                                        height: 30.h,
+                                        width: 30.w,
+                                        decoration: BoxDecoration(
+                                            color: state.coLeaderIndex == index ? AppColors.gradientOne : AppColors.white,
+                                            shape: BoxShape.circle,
+                                            boxShadow: [BoxShadow(color: AppColors.blue, spreadRadius: 1.0, blurRadius: 1.0)],
+                                            border: Border.all(color: AppColors.blue)),
+                                        child: Center(
+                                          child: TextView(
+                                            text: "CL",
+                                            fontSize: 15.sp,
+                                            fontWeight: FontWeight.bold,
+                                            fontColor: state.coLeaderIndex == index ? AppColors.white : AppColors.color666666,
+                                          ),
+                                        ));
+                                  }
+                                  return Container(
+                                      height: 30.h,
+                                      width: 30.w,
+                                      decoration: BoxDecoration(
+                                          color: AppColors.white,
+                                          shape: BoxShape.circle,
+                                          boxShadow: [BoxShadow(color: AppColors.blue, spreadRadius: 1.0, blurRadius: 1.0)],
+                                          border: Border.all(color: AppColors.blue)),
+                                      child: Center(
+                                        child: TextView(
+                                          text: "CL",
+                                          fontSize: 15.sp,
+                                          fontWeight: FontWeight.bold,
+                                          fontColor: AppColors.color666666,
+                                        ),
+                                      ));
+                                },
+                              ),
                             )
                           ],
                         ).paddingSymmetric(horizontal: 5.w, vertical: 5.w)
@@ -186,13 +272,17 @@ class CommonStockSelectionCard extends StatelessWidget {
                                 size: 35.sp,
                               ),
                             ),
-                            SizedBox(width: 2.w,),
+                            SizedBox(
+                              width: 2.w,
+                            ),
                             GestureDetector(
                                 onTap: onMoreTap,
                                 child: CommonIcon(
                                   icon: Icons.more_horiz,
                                 )),
-                            SizedBox(width: 2.w,),
+                            SizedBox(
+                              width: 2.w,
+                            ),
                             GestureDetector(
                               onTap: onDownButtonTap,
                               child: Icon(
@@ -214,15 +304,8 @@ class CommonStockSelectionCard extends StatelessWidget {
               constraints: BoxConstraints(minWidth: 50.w, maxWidth: 120.w),
               decoration: BoxDecoration(
                   color: AppColors.white,
-                  boxShadow: [
-                    BoxShadow(
-                        color: AppColors.lightRed,
-                        blurRadius: 1.0,
-                        spreadRadius: 1.0)
-                  ],
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(10.r),
-                      bottomRight: Radius.circular(10.r))),
+                  boxShadow: [BoxShadow(color: AppColors.lightRed, blurRadius: 1.0, spreadRadius: 1.0)],
+                  borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10.r), bottomRight: Radius.circular(10.r))),
               child: TextView(
                 text: data['stocks'][index]['stockName'],
                 fontColor: AppColors.black,
